f30710e0fbee8c2a0ae95862b20683f5b02cb7a0
Mohammed Rayan sailani <rayanbackup1@gmail.com>
2021-09-21 20:24:40 +0530

    deleted some files

--- js/exelToJson.js
+++
@@ -1,9 +0,0 @@
-const readXlsxFile = require("read-excel-file/node");
-const fs = require("fs");
-// File path.
-readXlsxFile("./js/links.xlsx").then((rows) => {
-  // `rows` is an array of rows
-  // each row being an array of cells.
-  console.log(rows);
-  fs.writeFileSync("./output.json", JSON.stringify(rows));
-});
