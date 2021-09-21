d4cca9c6a8bd684b6fa368840920eed402c02691
Mohammed Rayan sailani <rayanbackup1@gmail.com>
2021-09-21 20:22:58 +0530

    changed some HTML and styling

--- js/exelToJson.js
+++ js/exelToJson.js
@@ -1,14 +1,9 @@
-const readXlsxFile = require('read-excel-file/node')
-const fs = require('fs')
+const readXlsxFile = require("read-excel-file/node");
+const fs = require("fs");
 // File path.
-readXlsxFile('./js/links.xlsx').then((rows) => {
+readXlsxFile("./js/links.xlsx").then((rows) => {
   // `rows` is an array of rows
   // each row being an array of cells.
-  console.log(rows)
-  fs.writeFileSync("./output.json", JSON.stringify(rows))
-})
-
-// // Readable Stream.
-// readXlsxFile(fs.createReadStream('./links.xlsx')).then((rows) => {
-//     console.log(rows)
-// })
+  console.log(rows);
+  fs.writeFileSync("./output.json", JSON.stringify(rows));
+});
